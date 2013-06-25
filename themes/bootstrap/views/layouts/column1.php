eed6d614cd0d3126ce9d0fb99c35f244a1015342
Nguyen Dinh Trung <nguyendinhtrung141@gmail.com>
2013-06-25 12:32:41 +0700

    Update theme

--- themes/bootstrap/views/layouts/column1.php
+++ themes/bootstrap/views/layouts/column1.php
@@ -1,6 +1,7 @@
 <?php $this->beginContent('//layouts/main'); ?>
 <div class="container">
 	<div id="content">
+		<?php if (! empty($this->menu)) echo TbHtml::buttonGroup($this->menu); ?>
 		<?php echo $content; ?>
 	</div><!-- content -->
 </div>
--- themes/bootstrap/views/layouts/main.php
+++ themes/bootstrap/views/layouts/main.php
@@ -5,11 +5,10 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
 
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css" />
 
 	<title><?php echo CHtml::encode(strip_tags($this->pageTitle)); ?></title>
-
 	<?php Yii::app()->bootstrap->register(); ?>
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css" />
 </head>
 
 <body>
